8e3430e1416a9a986f16fa289eff0d1ef8d6c44c
seifer08ms <hi181904665@gmail.com>
2016-10-14 01:08:52 +0800

    update  document and example code of function zonalstats()

--- R/zonalstats.R
+++ R/zonalstats.R
@@ -2,48 +2,38 @@
 #'
 #' This function calculate statistics for each zone defined by a zone dataset.
 #' @import data.table
+#' @param x the raster dataset to be calculated
 #' @param zonal.ras a raster objects indicating each zone
-#' @param val.ras the raster dataset to be calculated
-#' @param stat character.statistics method to be performed
-#' @param extent extent object.Spatial extent of output raster.If missing, the extent of x will be used.
+#' @param stat character.statistics method to be performed.The defautl value is 'mean'.
+#' @param digit integer indicating the number of decimal places (round) for zone id.The default value is 0.
+#' @param na.rm logic indicating whether to remove NA value during the zonal statistic.The default value is true
+#' @param prefix character indicating column name prefix of zonal statistical value.
 #' @export
 #' @return RasterLayer
 #' @examples
-#' ###############################
-#' # rasterize points
-#' ###############################
-#' r <- raster(ncols=36, nrows=18)
-#' n <- 1000
-#' x <- runif(n) * 360 - 180
-#' y <- runif(n) * 180 - 90
-#' xy <- cbind(x, y)
-#' # with a SpatialPointsDataFrame
-#' vals <- 1:n
-#' p <- data.frame(xy, name=vals)
-#' coordinates(p) <- ~x+y
-#' ## If you are using windows,you must set local GDAL installation options before running function###
-#' #Not run:
-#' #gdal_setInstallation(search_path = 'the path of OSGeo4W64/bin',rescan = T)
-#' r2 <- rasterize.gdal(p, field = 'name', res =res(r)[1],extent=extent(r))
-zonalStat<-function(zonal.ras,val.ras,stat='mean',digits=0,na.rm=T,...){
+#' gg<-loadGRID()
+#' res.test<-1e4
+#' zonal.ras<-rasterize.gdal(china.pop[,'county_ID'],'county_ID',res=res.test)
+#' grid.poly.proj<-gg%>%spTransform(proj4string(china.pop))
+#' o3.test1<-rasterize.gdal(grid.poly.proj,names(grid.poly.proj)[2],res=res.test,
+#'                          extent=extent(china.pop))
+#' zonal.test<-zonalStat(zonal.ras,val.ras =o3.test1)
+#' ##############  batch processing ######
+#' o3.stack.3<-lapply(names(grid.poly.proj)[-1],rasterize.gdal,
+#' x=grid.poly.proj,res=res.test,extent=extent(china.pop))
+#' zonal.res.all<-lapply(X=o3.stack.3,FUN = zonalStat,zonal.ras=zonal.ras)
+
+zonalStat<-function(x,zonal.ras,stat='mean',digits=0,na.rm=T,prefix='zonal',...){
 
     library(data.table)
     library(raster)
     fun <- match.fun(stat)
-    vals <- getValues(val.ras)
+    vals <- getValues(x)
     zones <- round(getValues(zonal.ras), digits = digits)
     rDT <- data.table(vals, z=zones)
     setkey(rDT, z)
     Zstat<-data.frame(rDT[, lapply(.SD, fun,na.rm=na.rm,...), by=z])
-    colnames(Zstat)[2:length(Zstat)]<-
-        paste0("B", c(1:(length(Zstat)-1)), "_",stat)
+    colnames(Zstat)[2:length(Zstat)]<-paste(prefix,stat,sep="_")
     return (Zstat)
 }
-# myZonal <- function (x, z, stat, digits =0, na.rm = TRUE,
-#                      ...) {
-#     # x<-o3.test1
-#     # z<-zonal.ras
-#     # digits<-0
-#     # stat<-'mean'
-#
-# }
+
