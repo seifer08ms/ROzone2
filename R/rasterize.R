4459c0a39e603a11cbe3c039c3f7be1e08b00352
seifer08ms <hi181904665@gmail.com>
2016-10-18 09:47:40 +0800

    change param x to param obj for rasterize.gdal function()

--- R/rasterize.R
+++ R/rasterize.R
@@ -1,7 +1,7 @@
 #' High performance rasterize function using gdalUtils.
 #'
 #' This function rasterize a vector data by gdal binary utils. This is a wrap function of gdalUtils::gdal_rasterize.
-#' @param x a Spatial*DataFrame objects.
+#' @param obj a Spatial*DataFrame objects.
 #' @param field character.The column name of the variable to be transferred
 #' @param res numeric.Resolution of the output raster
 #' @param extent extent object.Spatial extent of output raster.If missing, the extent of x will be used.
@@ -24,22 +24,23 @@
 #' #Not run:
 #' #gdal_setInstallation(search_path = 'the path of OSGeo4W64/bin',rescan = T)
 #' r2 <- rasterize.gdal(p, field = 'name', res =res(r)[1],extent=extent(r))
-rasterize.gdal<-function(x,field,res=1e4,extent=NULL){
+rasterize.gdal<-function(obj,field,res=1e4,extent=NULL){
     library(rgdal)
     library(raster)
     src_datasource.dsn<-file.path(tempdir(),'zonal.shp')
     if(file.exists(src_datasource.dsn)) file.remove(src_datasource.dsn)
-    writeOGR(x,dsn = src_datasource.dsn,driver = 'ESRI Shapefile',layer = 'zonal',
+    writeOGR(obj,dsn = src_datasource.dsn,driver = 'ESRI Shapefile',layer = 'zonal',
              overwrite_layer = T)
     if(is.null(extent)){
-        extent.x<-extent(x)
+        extent.x<-extent(obj)
     }else{
         extent.x<-extent
     }
     dst_filename.dsn<-file.path(tempdir(),paste0(field,'.img') )
-    gdalUtils::gdal_rasterize(src_datasource.dsn,dst_filename = dst_filename.dsn,a=field,
-                   te =c(extent.x@xmin,extent.x@ymin,
-                         extent.x@xmax,extent.x@ymax),
+    gdalUtils::gdal_rasterize(src_datasource.dsn,dst_filename = dst_filename.dsn,
+                              a=field,
+                              te =c(extent.x@xmin,extent.x@ymin,
+                                    extent.x@xmax,extent.x@ymax),
                    a_nodata=32768,of='HFA',tr=c(res,res),verbose=T,output_Raster=T)
     raster(dst_filename.dsn)
 }
